bb4e267d347209dacbf0b3d85f4a9e80b7659a27
Alex <112809121+Axtrkv@users.noreply.github.com>
2022-12-30 11:39:42 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const removeBtn = document.querySelector("#removeBtn");
 const list = document.querySelector("#list")
 const listItem = document.querySelectorAll("#listItem")
+const div = document.querySelectorAll("#div.list")
 
 
 removeBtn.addEventListener('click', clean);
 function clean(){
-    list.remove(listItem)
+    list.remove(div)
 }
