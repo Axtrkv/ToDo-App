e487ac7396c58e06a07af93e60b9571675d7c366
Alex <112809121+Axtrkv@users.noreply.github.com>
2022-12-30 22:05:51 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,45 @@
 const removeBtn = document.querySelector("#removeBtn");
-const list = document.querySelector("#list")
-const listItem = document.querySelectorAll("#div.listItem")
-const div = document.querySelectorAll("#div.list")
+const addBtn = document.querySelector("#addBtn");
+const inputElement = document.querySelector("#example");
+const list = document.querySelector("#list");
+const listCheckBtn = document.querySelector("#check");
+const listRemoveBtn = document.querySelector("#remove");
 
-
-// removeBtn.addEventListener('click', clean);
-// function clean(){
-//     list.removeChild(listItem)
-// }
-
-removeBtn.addEventListener('click', clean);
-function clean(){
-    list.innerHTML = '';
+// Whole list cleaner
+removeBtn.addEventListener("click", clean);
+function clean() {
+  list.innerHTML = "";
 }
+
+// Adding new task
+addBtn.addEventListener("click", () => {
+  let newDiv = document.createElement("newDiv");
+  newDiv.id = newDiv;
+  list.appendChild(newDiv);
+  newDiv.className = "listItem";
+
+  let newElement = document.createElement("newElement");
+  newElement.className = "Task";
+  newDiv.appendChild(newElement);
+  newElement.innerHTML = inputElement.value;
+
+  if (inputElement.value.length < 1) {
+    alert ('Field can`t be empty')
+    newDiv.remove()
+  }
+
+  let newCheckEl = document.createElement("newCheckEl");
+  newCheckEl.className = "check";
+  newCheckEl.type = "button";
+  newDiv.appendChild(newCheckEl);
+
+  let newRemoveEl = document.createElement("newRemoveEl");
+  newRemoveEl.className = "remove";
+  newRemoveEl.type = "button";
+  newDiv.appendChild(newRemoveEl);
+});
+
+// Task remove button 
+listRemoveBtn.addEventListener("click", () => {
+    newDiv.removeChild()
+});
