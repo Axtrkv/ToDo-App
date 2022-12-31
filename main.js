ec3914e0ff8b68abbfadd996d27d1c6f0ee5d7c5
Alex <112809121+Axtrkv@users.noreply.github.com>
2022-12-31 17:22:34 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const inputElement = document.querySelector("#example");
 const list = document.querySelector("#list");
 const listCheckBtn = document.querySelector("#check");
 const listRemoveBtn = document.querySelector("#remove");
+const listItem = document.querySelector('#listItem')
 
 // Whole list cleaner
 removeBtn.addEventListener("click", clean);
@@ -11,12 +12,23 @@ function clean() {
   list.innerHTML = "";
 }
 
+// Remove old tasks (???)
+listRemoveBtn.addEventListener('click', () => {
+  list.removeChild(listItem)
+})
+
+//Mark old tasks (???)
+listCheckBtn.addEventListener('click', () => {
+  listCheckBtn.classList.add('inProcess')
+})
+
 // Adding new task
 addBtn.addEventListener("click", () => {
   let newDiv = document.createElement("newDiv");
   newDiv.id = newDiv;
   list.appendChild(newDiv);
   newDiv.className = "listItem";
+  newDiv.id = 'newDiv'
 
   let newElement = document.createElement("newElement");
   newElement.className = "Task";
@@ -37,9 +49,15 @@ addBtn.addEventListener("click", () => {
   newRemoveEl.className = "remove";
   newRemoveEl.type = "button";
   newDiv.appendChild(newRemoveEl);
-});
+  
+  // Remove each element
+  newRemoveEl.addEventListener('click', () => {
+    list.removeChild(newDiv)
+  })
 
-// Task remove button 
-listRemoveBtn.addEventListener("click", () => {
-    newDiv.removeChild()
-});
+    // Mark if task is done
+  newCheckEl.addEventListener('click', () => {
+  newCheckEl.classList.add('inProcess')
+  newDiv.classList.add('taskInProcess')
+  })
+})
