11c457a22fb82dfc732586bdb956a40b79b42802
Alex <112809121+Axtrkv@users.noreply.github.com>
2023-01-31 18:52:51 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const inputElement = document.querySelector("#example");
 const list = document.querySelector("#list");
 const listCheckBtn = document.querySelector("#check");
 const listRemoveBtn = document.querySelector("#remove");
-const listItem = document.querySelector('#listItem')
+const listItem = document.querySelector("#listItem");
 
 // Whole list cleaner
 removeBtn.addEventListener("click", clean);
@@ -12,15 +12,15 @@ function clean() {
   list.innerHTML = "";
 }
 
-// Remove old tasks
-listRemoveBtn.addEventListener('click', () => {
-  list.removeChild(listItem)
-})
+// Remove old task
+listRemoveBtn.addEventListener("click", () => {
+  list.removeChild(listItem);
+});
 
-//Mark old tasks
-listCheckBtn.addEventListener('click', () => {
-  listCheckBtn.classList.add('inProcess')
-})
+//Mark old task
+listCheckBtn.addEventListener("click", () => {
+  listCheckBtn.classList.add("inProcess");
+});
 
 // Adding new task
 addBtn.addEventListener("click", () => {
@@ -28,7 +28,7 @@ addBtn.addEventListener("click", () => {
   newDiv.id = newDiv;
   list.appendChild(newDiv);
   newDiv.className = "listItem";
-  newDiv.id = 'newDiv'
+  newDiv.id = "newDiv";
 
   let newElement = document.createElement("newElement");
   newElement.className = "Task";
@@ -36,29 +36,33 @@ addBtn.addEventListener("click", () => {
   newElement.innerHTML = inputElement.value;
 
   if (inputElement.value.length < 1) {
-    alert ('Field can`t be empty')
-    newDiv.remove()
+    alert("Field can`t be empty");
+    newDiv.remove();
+  } else if (inputElement.value.length > 40) {
+    alert("You reached maximum length(max. 40)");
+    newDiv.remove();
+    inputElement.value = "";
   }
 
   let newCheckEl = document.createElement("newCheckEl");
   newCheckEl.className = "check";
   newCheckEl.type = "button";
   newDiv.appendChild(newCheckEl);
-  newCheckEl.title = 'Mark if done'
+  newCheckEl.title = "Mark if done";
 
   let newRemoveEl = document.createElement("newRemoveEl");
   newRemoveEl.className = "remove";
   newRemoveEl.type = "button";
   newDiv.appendChild(newRemoveEl);
-  
-  // Remove each element
-  newRemoveEl.addEventListener('click', () => {
-    list.removeChild(newDiv)
-  })
 
-    // Mark if task is done
-  newCheckEl.addEventListener('click', () => {
-  newCheckEl.classList.add('inProcess')
-  newDiv.classList.add('taskInProcess')
-  })
-})
+  // Remove each element
+  newRemoveEl.addEventListener("click", () => {
+    list.removeChild(newDiv);
+  });
+
+  // Mark if task is done
+  newCheckEl.addEventListener("click", () => {
+    newCheckEl.classList.add("inProcess");
+    newDiv.classList.add("taskInProcess");
+  });
+});
